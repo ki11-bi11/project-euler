cf40568224f6df3187ec50ac689d8d79913f8e29
ki11_bi11 <billdattilio@gmail.com>
2022-03-28 14:46:54 -0400

    p044

--- src/main/kotlin/utils/PolygonalNumbers.kt
+++ src/main/kotlin/utils/PolygonalNumbers.kt
@@ -8,7 +8,24 @@ object PolygonalNumbers {
     }
 
     fun isTriangular(t_n: Int): Boolean {
+        return isTriangular(t_n.toLong())
+    }
+
+    fun isTriangular(t_n: Long): Boolean {
         val n = (1 + sqrt(8 * t_n + 1.0)) / 2.0
+        return n % 1.0 == 0.0
+    }
+
+    fun nthPentagonal(n: Int): Long {
+        return n.toLong() * (3 * n - 1) / 2
+    }
+
+    fun isPentagonal(p_n: Int): Boolean {
+        return isPentagonal(p_n.toLong())
+    }
+
+    fun isPentagonal(p_n: Long): Boolean {
+        val n = (1 + sqrt(24 * p_n + 1.0)) / 6.0
         return n % 1.0 == 0.0
     }
 }
